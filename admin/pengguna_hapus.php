5ba51087f5b23608a3fc25c0cac302ac9f327c38
GusErlangga <baguserlangga96@gmail.com>
2016-12-27 22:58:48 +0800

    hapus pengguna_hapus.php mau

--- admin/pengguna_hapus.php
+++ admin/pengguna_hapus.php
@@ -2,10 +2,11 @@
 	include '../config.php';
 
 	$user=$_GET['user'];
-	$sql = "DELETE FROM user WHERE user=$user";
+	$sql = "DELETE FROM user WHERE user='$user'";
 
 	if (mysqli_query($link, $sql)) {
-	    header("Location: pengguna.php");
+	    //header("Location: pengguna.php");
+
 	} else {
 	    echo "Error deleting record: " . mysqli_error($link);
 	}
